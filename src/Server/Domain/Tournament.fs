5f42aec3beb29aa6d30e606945ebef19dd812b09
Robert Oelke <Robert.Oelke@lmis.de>
2020-01-13 14:11:34 +0100

    AsyncResult von swlaschin eingebaut.

--- src/Server/Domain/Tournament.fs
+++ src/Server/Domain/Tournament.fs
@@ -1,5 +1,9 @@
 module Tournament
+
 open System
+open FSharp
+open Shared.ResultBuilder
+open Shared
 
 type TournamentId = TournamentId of int
 
@@ -35,9 +39,40 @@ type TorunamentInCreationPhase = {
 
 type TeamCreationError =
 | PlayerAlreadyExists of Player
+| WrongTitleFormat of string
+| WrongStartDate of string
 
 type ParseNewTournamentDto = Shared.Dtos.NewTournament -> Result<NewTournament, TeamCreationError>
 
 type CreateTournament = NewTournament -> Result<TorunamentInCreationPhase, TeamCreationError>
 
 type AddTeam = TorunamentInCreationPhase -> Team -> Result<TorunamentInCreationPhase, TeamCreationError>
+
+let validatedTitle title =
+  match title with
+  | title when String.IsNullOrWhiteSpace title ->
+      "Der Titel darf nicht leer sein." |> WrongTitleFormat |> Error
+
+  | title when title.Length > 50 ->
+      "Der Titel dar maximal 50 Zeichen lang sein." |> WrongTitleFormat |> Error
+
+  | title -> Ok title
+
+let validatedStartDate startDate =
+  match startDate with
+  | date when date < DateTime.Today ->
+      "Das Datum darf nicht in der Vergangenheit liegen" |> WrongStartDate |> Error
+
+  | date -> Ok date
+
+
+let parseNewTournamentDto (newTournamentDto: Shared.Dtos.NewTournament) =
+
+  let r = asyncResult {
+    let! title = newTournamentDto.Title |> validatedTitle |> AsyncResult.ofResult
+    let! startDate = newTournamentDto.StartDate |> validatedStartDate  |> AsyncResult.ofResult
+
+    return 1
+  }
+
+  ()
